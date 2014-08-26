c116272fd48950c5f00ae4eef87be881b46142d3
Joshua Hodosh <jhodosh@users.noreply.github.com>
2014-08-26 15:15:24 -0400

    oops

--- qemu/translate-all.c
+++ qemu/translate-all.c
@@ -207,9 +207,7 @@ int cpu_restore_state(TranslationBlock *tb,
     j = tcg_gen_code_search_pc(s, (uint8_t *)tc_ptr, searched_pc - tc_ptr);
 #ifdef CONFIG_LLVM
     }
-#e#ifdef CONFIG_LLVM
-    }
-#endifndif
+#endif
     if (j < 0)
         return -1;
     /* now find start of instruction before */
